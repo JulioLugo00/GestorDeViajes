02f6e97cd204ffec5b32a686d9e9052c0700df7e
ZabdielV <A01377950@itesm.mx>
2021-10-03 13:45:12 -0500

    Primer avanze de proyecto. *Se agrego un menú *Interfaz gráfica *Pantalla de carga (simulado)

--- app/src/main/java/mx/itesm/noobmasters/gestordeviajes/MainActivity.kt
+++ app/src/main/java/mx/itesm/noobmasters/gestordeviajes/MainActivity.kt
@@ -1,11 +1,36 @@
 package mx.itesm.noobmasters.gestordeviajes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Esconde el titulo de la app
+        supportActionBar?.hide()
+
+        cargarPrincipal()
+    }
+
+    private fun cargarPrincipal() {
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            cambiarPantalla()
+
+
+        },5000)
+    }
+
+    private fun cambiarPantalla() {
+        val intPrincipal = Intent(this, MainActivity2::class.java)
+        startActivity(intPrincipal)
+        //Destruye la pantalla, esta actividad (Slash Activity)
+        this.finish()
     }
 }
